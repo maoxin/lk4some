f6404947e9b268eef68170685d15eb206746acf4
maoxin <maoxin.horizon@gmail.com>
2018-04-12 15:16:37 +0800

    fix a bug
    * lower the str of chosen color before search

--- find_by_color.py
+++ find_by_color.py
@@ -26,7 +26,7 @@ class ColorSelector(object):
         # load color table, parse chosen color
         with open('color_table.yml', 'r') as f:
             self.color_table = yaml.load(f.read())
-        self.chosen_color_name = chosen_color_name.replace("色", "")
+        self.chosen_color_name = chosen_color_name.replace("色", "").lower()
         if self.chosen_color_name not in self.color_table['color_list']:
             try:
                 self.chosen_color_name = self.color_table['translate'][self.chosen_color_name]
